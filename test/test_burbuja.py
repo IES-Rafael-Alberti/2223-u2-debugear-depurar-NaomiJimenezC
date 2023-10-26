80e46a49629563e2995ada922669eb1d287413cd
IgnacioJimenezC <ijimenez@iesrafaelalberti.es>
2023-10-26 18:20:21 +0200

    inicio del burbuja hecho

--- test/test_burbuja.py
+++ test/test_burbuja.py
@@ -1,4 +1,3 @@
-from src.main import * #cambiar esto
 import pytest
 
 def aplicar_burbuja(lista_a_ordenar:list)->list:
@@ -7,3 +6,12 @@ def aplicar_burbuja(lista_a_ordenar:list)->list:
 
 def test_comprobar_si_ordena_bien_el_burbujas():
     assert aplicar_burbuja([5,4,3,2,1]) == [1,2,3,4,5]
+    
+
+a = [5,4,3,2,1]
+#analizalo luego
+for iteracion_padre in range(len(a)-1):
+    for iteracion_hijo in range(len(a)-1-iteracion_padre):
+        if a[iteracion_hijo] > a[iteracion_hijo+1]:
+            a[iteracion_hijo] , a[iteracion_hijo+1] = a[iteracion_hijo+1] , a[iteracion_hijo]
+print(a)
