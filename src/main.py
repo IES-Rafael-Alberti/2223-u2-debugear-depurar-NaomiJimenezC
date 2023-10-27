e87d037686937273798a553b96a5ca8603051cdd
IgnacioJimenezC <ijimenez@iesrafaelalberti.es>
2023-10-27 19:33:26 +0200

    final

--- src/main.py
+++ src/main.py
@@ -4,6 +4,18 @@ hacer el algoritmo burbuja
 
 #entrada
 
+def ingresar_valores() -> str:
+    lista_valor = [ ]
+    valor = input("Ingrese un valor (Ingrese un input vacío cuando quieras parar): ")
+    while valor != "":
+        lista_valor.append(valor)
+        valor = input("Ingrese un valor (Ingrese un input vacío cuando quieras parar): ")
+    if comprobar_si_todos_los_elementos_son_del_mismo_tipo(lista_valor):
+        return lista_valor    
+        
+
+
+
 #procesado
 def comprobar_si_todos_los_elementos_son_del_mismo_tipo(lista_a_comprobar:list)->bool:
     """comprobar si todos los elementos son del mismo tipo
@@ -25,8 +37,21 @@ def comprobar_si_todos_los_elementos_son_del_mismo_tipo(lista_a_comprobar:list)-
         raise ValueError("No todos los elementos son del mismo tipo:")
 
 
+def aplicar_burbuja(lista_a_ordenar:list):
+    for iteracion_padre in range(len(lista_a_ordenar)-1):
+        for iteracion_hijo in range(len(lista_a_ordenar)-1-iteracion_padre):
+            if lista_a_ordenar[iteracion_hijo] > lista_a_ordenar[iteracion_hijo+1]:
+                lista_a_ordenar[iteracion_hijo] , lista_a_ordenar[iteracion_hijo+1] = lista_a_ordenar[iteracion_hijo+1] , lista_a_ordenar[iteracion_hijo]
+    return lista_a_ordenar
 
 #salida
 
+def mostrar_lista_ordenada(lista_ordenada:list)->str:
+    return f"La lista de elementos ordenadas queda así: {lista_ordenada}"
+
 if __name__ == "__main__":
-    print()
+    lista_valores = ingresar_valores()
+    lista_ordenada = aplicar_burbuja(lista_valores)
+    resultado = mostrar_lista_ordenada(lista_ordenada)
+    
+    print(resultado)
--- test/test_burbuja.py
+++ test/test_burbuja.py
@@ -1,17 +1,10 @@
 import pytest
 
-def aplicar_burbuja(lista_a_ordenar:list)->list:
-    
-    return [1,2,3,4,5]
+
+
 
 def test_comprobar_si_ordena_bien_el_burbujas():
     assert aplicar_burbuja([5,4,3,2,1]) == [1,2,3,4,5]
-    
 
-a = [5,4,3,2,1]
-#analizalo luego
-for iteracion_padre in range(len(a)-1):
-    for iteracion_hijo in range(len(a)-1-iteracion_padre):
-        if a[iteracion_hijo] > a[iteracion_hijo+1]:
-            a[iteracion_hijo] , a[iteracion_hijo+1] = a[iteracion_hijo+1] , a[iteracion_hijo]
-print(a)
+def test_ordena_un_elemento():
+    assert aplicar_burbuja([1]) == [1]
